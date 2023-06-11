28db759416ad0c532e9d9583a98ef70c96f6dbc9
ddbriggs95 <dowlingbriggs@gmail.com>
2023-06-11 16:04:24 -0500

    test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import About from './components/About';
 import Contact from './components/Contact';
 import Portfolio from './components/Portfolio';
 import Resume from './components/Resume';
-import Navbar from './components/Navbar';
 import NavbarFunc from "./components/Navbar";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,12 +1,12 @@
 import React from "react";
-//import resume from '../assets/'
+import resume from '../assets/Briggs_Dowling_CV.pdf'
 function Resume() {
 
     return (
         <div>Click here to download my resume
-            {/* <a href={resume} download>
+            <a href={resume} download>
                 <button>Download</button>
-            </a> */}
+            </a>
 
         </div>
     )
